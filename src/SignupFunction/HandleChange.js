a01d9de6b010a1d2e9449093a002fe4b4aaf8b5f
Venomv252 <aryanraj9931614200@gmail.com>
2025-11-27 22:49:29 +0530

    chamging url

--- src/SignupFunction/HandleChange.js
+++ src/SignupFunction/HandleChange.js
@@ -40,7 +40,7 @@ export const  handleSignupservice = async (
   
 
   try {
-    const res = await fetch("http://localhost:5000/api/signup", {
+    const res = await fetch("https://rapid-resq-backend.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
