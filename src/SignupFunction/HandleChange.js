3d620daf7a1307961294e2dee1816b76c59c4263
Venomv252 <aryanraj9931614200@gmail.com>
2025-12-17 20:02:24 +0530

    update

--- src/SignupFunction/HandleChange.js
+++ src/SignupFunction/HandleChange.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const validation = (formData, setError) => {
-  if (!formData.name || !formData.email || !formData.phoneNumber) {
+  if (!formData.name || !formData.email || !formData.Phone_number) {
     setError("All fields are required.");
     return false;
   }
